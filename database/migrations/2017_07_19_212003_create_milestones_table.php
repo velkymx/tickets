879a63fe4d3e638528ac1ed2d044116a789f4022
Alan Bollinger <abollinger@i-viewnow.com>
2017-07-19 14:23:14 -0700

    Milestones migration

---
+++ database/migrations/2017_07_19_212003_create_milestones_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMilestonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('milestones', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string('name');
+          $table->tinyInteger('active');
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('milestones');
+    }
+}
