d6ff9176b887efdab01c30df5ad429fff8982671
Alan B <abollinger@i-viewnow.com>
2017-09-18 13:41:19 -0700

    Add fillable options to milestone

--- app/Milestone.php
+++ app/Milestone.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Milestone extends Model
 {
+
+  public $timestamps = false;
+
+  protected $fillable = [
+      'name','description','start_at','due_at','end_at',
+  ];
+
   public function tickets()
   {
       return $this->hasMany('App\Ticket');
