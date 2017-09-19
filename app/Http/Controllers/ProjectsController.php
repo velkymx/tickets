08fb4094e9b426a258bad3401e39577239666337
Alan B <abollinger@i-viewnow.com>
2017-09-19 16:13:39 -0700

    Beautify

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -10,102 +10,82 @@ class ProjectsController extends Controller
 {
     public function index()
     {
+        $projects = \App\Project::get();
 
-      $projects = \App\Project::get();
-
-      return view('projects.index',compact('projects'));
-
+        return view('projects.index', compact('projects'));
     }
 
     public function show($id)
     {
+        $project = \App\Project::findOrFail($id);
 
-      $project = \App\Project::findOrFail($id);
+        $perpage = 10;
 
-      $perpage = 10;
+        $filters = array('milestone_id','project_id','sprint_id','status_id','type_id','user_id','importance_id');
 
-      $filters = array('milestone_id','project_id','sprint_id','status_id','type_id','user_id','importance_id');
+        $queryfilter = array();
 
-      $queryfilter = array();
-
-      foreach($filters as $filter){
-
-        if(isset($request->$filter) && is_numeric($request->$filter)){
-
-          $queryfilter[$filter] = $request->$filter;
-
+        foreach ($filters as $filter) {
+            if (isset($request->$filter) && is_numeric($request->$filter)) {
+                $queryfilter[$filter] = $request->$filter;
+            }
         }
 
-      }
+        if (is_array($queryfilter) && sizeof($queryfilter)>0) {
+            $tickets = new \App\Ticket;
 
-      if(is_array($queryfilter) && sizeof($queryfilter)>0){
+            foreach ($queryfilter as $filter => $value) {
+                $tickets = $tickets->where($filter, $value);
+            }
 
-          $tickets = new \App\Ticket;
+            $tickets = $tickets->paginate($perpage);
+        } else {
+            $tickets = \App\Ticket::where('project_id', $project->id)->paginate($perpage);
+        }
 
-          foreach ($queryfilter as $filter => $value) {
+        $statuscodes = \App\Status::get();
 
-            $tickets = $tickets->where($filter,$value);
+        $percent = 0;
 
-          }
+        $completed = $project->tickets()->whereNotIn('status_id', ['5','8','9'])->count();
 
-          $tickets = $tickets->paginate($perpage);
+        $total = $project->tickets->count();
 
-      } else {
+        if ($total !== 0 && $completed !== 0) {
+            $percent = 100-(round($completed / $total, 2)*100);
+        }
 
-        $tickets = \App\Ticket::where('project_id',$project->id)->paginate($perpage);
-
-      }
-
-      $statuscodes = \App\Status::get();
-
-      $percent = 0;
-
-      $completed = $project->tickets()->whereNotIn('status_id',['5','8','9'])->count();
-
-      $total = $project->tickets->count();
-
-      if($total !== 0 && $completed !== 0){
-
-        $percent = 100-(round($completed / $total,2)*100);
-
-      }
-
-      return view('projects.show',compact('project','tickets','queryfilter','total','completed','percent','statuscodes'));
+        return view('projects.show', compact('project', 'tickets', 'queryfilter', 'total', 'completed', 'percent', 'statuscodes'));
     }
 
     public function create()
     {
-      return view('projects.create');
+        return view('projects.create');
     }
 
     public function edit(Request $request)
     {
-      $project = \App\Project::findOrFail($request->id);
+        $project = \App\Project::findOrFail($request->id);
 
-      return view('projects.edit',compact('project'));
+        return view('projects.edit', compact('project'));
     }
 
     public function store(Request $request)
     {
+        if ($request->id == 'new') {
+            $post = $request->toArray();
 
-      if($request->id == 'new'){
+            $post['active'] = 1;
 
-        $post = $request->toArray();
+            \App\Project::create($post);
+        } else {
+            $project = \App\Project::findOrFail($request->id);
 
-        $post['active'] = 1;
-
-        \App\Project::create($post);
-
-      } else {
-
-        $project = \App\Project::findOrFail($request->id);
-
-        $project->update($request->toArray());
-
-      }
+            $project->update($request->toArray());
+        }
 
 
 
-      return redirect('projects');
+        return redirect('projects');
     }
 }
--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -18,160 +18,129 @@ class TicketsController extends Controller
    *
    * @return void
    */
-  public function __construct()
-  {
-      $this->middleware('auth');
-  }
-
-    public function index(Request $request){
-
-      $perpage = 10;
-
-      $filters = array('milestone_id','project_id','sprint_id','status_id','type_id','user_id','importance_id');
-
-      $queryfilter = array();
-
-      foreach($filters as $filter){
-
-        if(isset($request->$filter) && is_numeric($request->$filter)){
-
-          $queryfilter[$filter] = $request->$filter;
-
-        }
-
-      }
-
-      if(is_array($queryfilter) && sizeof($queryfilter)>0){
-
-          $tickets = new Ticket;
-
-          foreach ($queryfilter as $filter => $value) {
-
-            $tickets = $tickets->where($filter,$value);
-
-          }
-
-          $tickets = $tickets->paginate($perpage);
-
-      } else {
-
-        $tickets = Ticket::paginate($perpage);
-
-      }
-
-      $lookups = $this->lookups();
-
-      $lookups['types'][0] = 'No Change';
-      $lookups['milestones'][0] = 'No Change';
-      $lookups['importances'][0] = 'No Change';
-      $lookups['projects'][0] = 'No Change';
-      $lookups['statuses'][0] = 'No Change';
-      $lookups['users'][0] = 'No Change';
-
-      return view('tickets.list',compact('tickets','queryfilter','lookups'));
-
+    public function __construct()
+    {
+        $this->middleware('auth');
     }
 
-    public function show($id){
+    public function index(Request $request)
+    {
+        $perpage = 10;
 
+        $filters = array('milestone_id','project_id','sprint_id','status_id','type_id','user_id','importance_id');
+
+        $queryfilter = array();
+
+        foreach ($filters as $filter) {
+            if (isset($request->$filter) && is_numeric($request->$filter)) {
+                $queryfilter[$filter] = $request->$filter;
+            }
+        }
+
+        if (is_array($queryfilter) && sizeof($queryfilter)>0) {
+            $tickets = new Ticket;
+
+            foreach ($queryfilter as $filter => $value) {
+                $tickets = $tickets->where($filter, $value);
+            }
+
+            $tickets = $tickets->paginate($perpage);
+        } else {
+            $tickets = Ticket::paginate($perpage);
+        }
+
+        $lookups = $this->lookups();
+
+        $lookups['types'][0] = 'No Change';
+        $lookups['milestones'][0] = 'No Change';
+        $lookups['importances'][0] = 'No Change';
+        $lookups['projects'][0] = 'No Change';
+        $lookups['statuses'][0] = 'No Change';
+        $lookups['users'][0] = 'No Change';
+
+        return view('tickets.list', compact('tickets', 'queryfilter', 'lookups'));
+    }
+
+    public function show($id)
+    {
         $ticket = Ticket::findOrFail($id);
 
         $lookups = $this->lookups();
 
-        return view('tickets.show',compact('ticket','lookups'));
-
+        return view('tickets.show', compact('ticket', 'lookups'));
     }
 
     public function create($value='')
     {
+        $lookups = $this->lookups();
 
-      $lookups = $this->lookups();
-
-      return view('tickets.create',compact('lookups'));
-
+        return view('tickets.create', compact('lookups'));
     }
 
     public function edit($id)
     {
-      $ticket = Ticket::findOrFail($id);
+        $ticket = Ticket::findOrFail($id);
 
-      $lookups = $this->lookups();
+        $lookups = $this->lookups();
 
-      return view('tickets.edit',compact('ticket','lookups'));
+        return view('tickets.edit', compact('ticket', 'lookups'));
     }
 
-    public function update(Request $request,$id)
+    public function update(Request $request, $id)
     {
 
       // $request = Request::all();
 
-      $ticket = Ticket::findOrFail($id);
+        $ticket = Ticket::findOrFail($id);
 
-      if(isset($request['due_at']) && $request['due_at'] <> ''){
-
-        $request['due_at'] = date('Y-m-d',strtotime($request['due_at']));
-
-      }
-
-      $changes = ['subject', 'description', 'type_id', 'status_id', 'importance_id', 'milestone_id', 'project_id', 'due_at', 'closed_at','estimate'];
-
-      $change_list = [];
-
-      foreach($changes as $change){
-
-        if($request->$change != $ticket->$change){
-
-          $label = $change;
-
-          if(substr($change,-3,3) == '_id'){
-
-            $label = substr($change,0,strlen($change)-3);
-
-            $change_list[] = ucwords($label).' changed to '.$ticket->$label->name;
-
-          } elseif(substr($change,-3,3) == '_at'){
-
-            $label = substr($change,0,strlen($change)-3);
-
-            $change_list[] = ucwords($label).' date changed to '.date('M jS, Y',strtotime($request->$change));
-
-          } else {
-
-            $change_list[] = ucwords($label).' changed to '.$request->$change;
-
-          }
-
+        if (isset($request['due_at']) && $request['due_at'] <> '') {
+            $request['due_at'] = date('Y-m-d', strtotime($request['due_at']));
         }
 
-      }
+        $changes = ['subject', 'description', 'type_id', 'status_id', 'importance_id', 'milestone_id', 'project_id', 'due_at', 'closed_at','estimate'];
 
-      $ticket->update($request->toArray());
+        $change_list = [];
 
-      $this->notate($ticket->id,'',$change_list);
+        foreach ($changes as $change) {
+            if ($request->$change != $ticket->$change) {
+                $label = $change;
 
-      \Session::flash('info_message','Ticket #'.$id.' updated');
+                if (substr($change, -3, 3) == '_id') {
+                    $label = substr($change, 0, strlen($change)-3);
 
-      return redirect('tickets/'.$id);
+                    $change_list[] = ucwords($label).' changed to '.$ticket->$label->name;
+                } elseif (substr($change, -3, 3) == '_at') {
+                    $label = substr($change, 0, strlen($change)-3);
 
+                    $change_list[] = ucwords($label).' date changed to '.date('M jS, Y', strtotime($request->$change));
+                } else {
+                    $change_list[] = ucwords($label).' changed to '.$request->$change;
+                }
+            }
+        }
+
+        $ticket->update($request->toArray());
+
+        $this->notate($ticket->id, '', $change_list);
+
+        \Session::flash('info_message', 'Ticket #'.$id.' updated');
+
+        return redirect('tickets/'.$id);
     }
 
     public function store(Request $request)
     {
+        $request = $request->toArray();
 
-      $request = $request->toArray();
+        $request['user_id'] = Auth::id();
 
-      $request['user_id'] = Auth::id();
+        if (isset($request['due_at']) && $request['due_at'] <> '') {
+            $request['due_at'] = date('Y-m-d', strtotime($request['due_at']));
+        }
 
-      if(isset($request['due_at']) && $request['due_at'] <> ''){
+        Ticket::create($request);
 
-        $request['due_at'] = date('Y-m-d',strtotime($request['due_at']));
-
-      }
-
-      Ticket::create($request);
-
-      return redirect('tickets');
-
+        return redirect('tickets');
     }
 
     public function upload(Request $request)
@@ -179,140 +148,110 @@ class TicketsController extends Controller
 
       // return $request->input('folder');
 
-      if(isset($_FILES) && sizeof($_FILES) > 0){
+        if (isset($_FILES) && sizeof($_FILES) > 0) {
+            $path = '/images/'.$request->input('folder').'/';
 
-        $path = '/images/'.$request->input('folder').'/';
+            if (!is_dir($path)) {
+                mkdir($_SERVER['DOCUMENT_ROOT'].$path);
+            }
 
-        if(!is_dir($path)){
+            move_uploaded_file($_FILES['file']['tmp_name'], $_SERVER['DOCUMENT_ROOT'].$path.$_FILES['file']['name']);
 
-            mkdir($_SERVER['DOCUMENT_ROOT'].$path);
-
+            return $path.$_FILES['file']['name'];
         }
-
-        move_uploaded_file($_FILES['file']['tmp_name'],$_SERVER['DOCUMENT_ROOT'].$path.$_FILES['file']['name']);
-
-        return $path.$_FILES['file']['name'];
-
-      }
-
     }
 
     public function batch(Request $request)
     {
-      $post = $request->toArray();
+        $post = $request->toArray();
 
-      $tickets = $post['tickets'];
+        $tickets = $post['tickets'];
 
-      unset($post['tickets']);
+        unset($post['tickets']);
 
-      if(count($tickets) == 0){
-
-        return redirect('tickets');
-
-      }
-
-      foreach($post as $k => $v){
-
-        if($v == 0){
-
-          unset($post[$k]);
-
+        if (count($tickets) == 0) {
+            return redirect('tickets');
         }
 
-      }
+        foreach ($post as $k => $v) {
+            if ($v == 0) {
+                unset($post[$k]);
+            }
+        }
 
-      $i = 0;
+        $i = 0;
 
-      foreach($tickets as $ticket){
+        foreach ($tickets as $ticket) {
+            $update = Ticket::findOrFail($ticket);
 
-        $update = Ticket::findOrFail($ticket);
+            $update->update($post);
 
-        $update->update($post);
+            $i++;
+        }
 
-        $i++;
+        \Session::flash('info_message', $i.' ticket(s) updated');
 
-      }
-
-      \Session::flash('info_message', $i.' ticket(s) updated');
-
-      return redirect('tickets');
-
+        return redirect('tickets');
     }
 
     public function board()
     {
-      $tickets = Ticket::get();
+        $tickets = Ticket::get();
 
-      $lookups = $this->lookups();
+        $lookups = $this->lookups();
 
-      return view('tickets.board',compact('tickets','lookups'));
+        return view('tickets.board', compact('tickets', 'lookups'));
     }
 
-    public function api(Request $request,$id)
+    public function api(Request $request, $id)
     {
+        $ticket = Ticket::findOrFail($id);
 
-      $ticket = Ticket::findOrFail($id);
+        if ($request['status'] != $ticket->status_id) {
+            $ticket->update(['status_id'=>$request['status']]);
 
-      if($request['status'] != $ticket->status_id){
+            $this->notate($ticket->id, '', ['Status Changed to '.$ticket->status->name]);
 
-        $ticket->update(['status_id'=>$request['status']]);
+            return 'Success';
+        }
 
-        $this->notate($ticket->id,'',['Status Changed to '.$ticket->status->name]);
-
-        return 'Success';
-
-      }
-
-      return 'Fail';
-
+        return 'Fail';
     }
 
     private function lookups()
     {
+        return array(
 
-      return array(
-
-        'types' => \App\Type::pluck('name','id'),
-        'milestones' => \App\Milestone::where('end_at',null)->pluck('name','id'),
-        'importances' => \App\Importance::pluck('name','id'),
-        'projects' => \App\Project::where('active',1)->pluck('name','id'),
-        'statuses' => \App\Status::pluck('name','id'),
-        'users' => \App\User::pluck('name','id')
+        'types' => \App\Type::pluck('name', 'id'),
+        'milestones' => \App\Milestone::where('end_at', null)->pluck('name', 'id'),
+        'importances' => \App\Importance::pluck('name', 'id'),
+        'projects' => \App\Project::where('active', 1)->pluck('name', 'id'),
+        'statuses' => \App\Status::pluck('name', 'id'),
+        'users' => \App\User::pluck('name', 'id')
 
       );
-
     }
 
-    private function notate($ticket_id,$message,$changes)
+    private function notate($ticket_id, $message, $changes)
     {
+        $insert['user_id'] = Auth::id();
+        $insert['ticket_id'] = $ticket_id;
+        $insert['body'] = $message;
 
-      $insert['user_id'] = Auth::id();
-      $insert['ticket_id'] = $ticket_id;
-      $insert['body'] = $message;
+        $change_list = '';
 
-      $change_list = '';
+        if (count($changes) > 0) {
+            foreach ($changes as $change) {
+                $change_list .= '<li>'.$change.'</li>';
+            }
 
-      if(count($changes) > 0){
-
-        foreach($changes as $change){
-
-          $change_list .= '<li>'.$change.'</li>';
-
+            if ($message <> '') {
+                $insert['body'] = $message.'<hr><ul>'.$change_list.'</ul>';
+            } else {
+                $insert['body'] = '<ul>'.$change_list.'</ul>';
+            }
         }
 
-        if($message <> ''){
-
-          $insert['body'] = $message.'<hr><ul>'.$change_list.'</ul>';
-
-        } else {
-
-          $insert['body'] = '<ul>'.$change_list.'</ul>';
-
-        }
-
-      }
-
-      \App\Note::create($insert);
-
+        \App\Note::create($insert);
     }
 }
