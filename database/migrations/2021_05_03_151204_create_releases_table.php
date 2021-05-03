96335e9135ad90a044639d40fcad3bc649aa612e
Alan B <velkymx@gmail.com>
2021-05-03 11:17:45 -0400

    Releases table

--- database/migrations/2021_05_03_151204_create_releases_table.php
+++ database/migrations/2021_05_03_151204_create_releases_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateReleaseTable extends Migration
+class CreateReleasesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,15 @@ class CreateReleaseTable extends Migration
      */
     public function up()
     {
-        Schema::create('release', function (Blueprint $table) {
+        Schema::create('releases', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title', 200);
+            $table->text('body')->nullable();
+            $table->dateTime('started_at')->nullable();
+            $table->dateTime('completed_at')->nullable();
+            $table->integer('user_id')->unsigned();
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
