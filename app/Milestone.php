4047786b59ff3e560b93408febcb98b86d197b27
AJ <velkymx@gmail.com>
2021-05-19 16:13:36 -0400

    Add sprint owner and scrum master

--- app/Milestone.php
+++ app/Milestone.php
@@ -10,11 +10,21 @@ class Milestone extends Model
   public $timestamps = false;
 
   protected $fillable = [
-      'name','description','start_at','due_at','end_at',
+      'name','description','scrummaster_user_id','owner_user_id','start_at','due_at','end_at',
   ];
 
   public function tickets()
   {
       return $this->hasMany('App\Ticket');
   }
+
+  public function owner()
+  {
+      return $this->hasOne('App\User','id','owner_user_id');
+  }
+  
+  public function scrummaster()
+  {
+      return $this->hasOne('App\User','id','scrummaster_user_id');
+  }  
 }
