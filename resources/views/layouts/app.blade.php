9a5b207ae1957c8079b4c940fd8364292dc8e890
AJ <velkymx@gmail.com>
2023-03-22 12:23:50 -0400

    Restore old layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,131 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title','Welcome') | Tickets!</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Fonts -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+    <link rel="icon" type="image/png" sizes="16x16" href="/tickets.png">
+    <!-- Styles -->
+    @if (Auth::guest())
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    @else
+    <link href="{{\Auth::user()->theme}}" rel="stylesheet">
+    @endif
+    <link href="/css/jquery-ui.min.css" rel="stylesheet">
+    <link href="/css/summernote.css" rel="stylesheet">
+    <link href="/css/overrides.css" rel="stylesheet">
+    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <style>
+        body {
+            font-family: 'Lato';
+        }
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+        .fa-btn {
+            margin-right: 6px;
+        }
+    </style>
+</head>
+<body id="app-layout">
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
 
-            <!-- Page Content -->
-            <main>
-            @yield('content')
-            </main>
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <span class="text-warning">Tickets</span>
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    <li><a href="{{ url('/home') }}">Home</a></li>
+                    <li><a href="{{ url('/tickets') }}">All Tickets</a></li>
+                    <li><a href="{{ url('/milestone') }}">Milestones</a></li>
+                    <li><a href="{{ url('/releases') }}">Releases</a></li>
+                    <li><a href="{{ url('/projects') }}">Projects</a></li>                    
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ url('/login') }}">Login</a></li>
+                        <li><a href="{{ url('/register') }}">Register</a></li>
+                    @else
+                      <li><a href="/tickets/create">New Ticket</a></li>
+                      <li><a href="/tickets/import">Import</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                              <li><a href="/users/{{Auth::id()}}">Profile</a></li>
+                              <li><a href="/user/edit">Edit Profile</a></li>
+                              <li><hr></li>
+                                <li>
+                                  <a href="{{ route('logout') }}"
+                                      onclick="event.preventDefault();
+                                               document.getElementById('logout-form').submit();">
+                                      Logout
+                                  </a>
+
+                                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                      {{ csrf_field() }}
+                                  </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+            </div>
         </div>
-    </body>
+    </nav>
+    <div class="container">
+
+      @if (Session::has('info_message'))
+        <div class="alert alert-info alert-dismissible" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          {{ Session::get('info_message') }}
+        </div>
+      @endif
+
+      @if (Session::has('error_message'))
+        <div class="alert alert-danger alert-dismissible" role="alert">
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          {{ Session::get('error_message') }}
+        </div>
+      @endif
+
+    @yield('content')
+  </div>
+  <div class="container" align="center" style="padding:10px">
+  <hr>
+  Tickets is the open source ticket tracker powered by <a href="https://laravel.com" target="_blank">Laravel</a>. <a href="https://github.com/velkymx/tickets" target="_blank">Check us out on Github.</a> Provided "as is" under the <a href="https://opensource.org/licenses/MIT" target="_blank">MIT License</a>
+  </div>
+
+    <!-- JavaScripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js"></script>
+    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+    @yield('javascript')
+</body>
 </html>
