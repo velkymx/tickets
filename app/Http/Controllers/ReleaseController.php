3e9edfc7bb2d37e276c04fe797d17cdfcc6df635
Alan B <ajb@wow.com>
2026-03-20 14:48:29 -0700

    Replace Session::flash with redirect->with in ReleaseController

--- app/Http/Controllers/ReleaseController.php
+++ app/Http/Controllers/ReleaseController.php
@@ -2,13 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Carbon\Carbon;
 use App\Models\Release;
 use App\Models\ReleaseTicket;
 use App\Models\Type;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Foundation\Console\Presets\React;
 
 class ReleaseController extends Controller
 {
@@ -16,14 +15,14 @@ class ReleaseController extends Controller
     {
         $this->middleware('auth');
     }
-    
+
     public function index()
-    {     
+    {
 
         $releases = Release::all();
 
-        return View('release/index',compact('releases'));
-        
+        return View('release/index', compact('releases'));
+
     }
 
     public function create()
@@ -36,8 +35,8 @@ class ReleaseController extends Controller
 
         $release = Release::findOrFail($request->id);
 
-        return View('release/edit',compact('release'));
-    }    
+        return View('release/edit', compact('release'));
+    }
 
     public function put(Request $request)
     {
@@ -47,37 +46,35 @@ class ReleaseController extends Controller
         $release->title = $request->title;
         $release->body = $request->body;
 
-        if ($request->started_at <> '') {
+        if ($request->started_at != '') {
             $release->started_at = date('Y-m-d', strtotime($request->started_at));
         } else {
             $release->started_at = '';
-        }   
-        
-        if ($request->completed_at <> '') {
+        }
+
+        if ($request->completed_at != '') {
             $release->completed_at = date('Y-m-d', strtotime($request->completed_at));
         } else {
             $release->completed_at = '';
-        }           
+        }
 
         $release->save();
 
-        \Session::flash('info_message', 'Release Saved');
-
-        return redirect('release/' . $release->id);    
-    }        
+        return redirect('release/'.$release->id)->with('info_message', 'Release Saved');
+    }
 
     public function show(Request $request)
     {
         $release = Release::findOrFail($request->id);
 
-        $release_tickets = ReleaseTicket::where('release_id',$release->id)->get();
+        $release_tickets = ReleaseTicket::where('release_id', $release->id)->get();
 
         $tickets = [];
         $projects = [];
 
-        foreach($release_tickets as $ticket){            
+        foreach ($release_tickets as $ticket) {
 
-            if(!array_key_exists($ticket->ticket->project_id,$projects)){
+            if (! array_key_exists($ticket->ticket->project_id, $projects)) {
 
                 $projects[$ticket->ticket->project_id]['project'] = $ticket->ticket->project->name;
             }
@@ -85,15 +82,15 @@ class ReleaseController extends Controller
             $projects[$ticket->ticket->project_id]['tickets'][$ticket->ticket->type->name][] = $ticket->ticket;
 
         }
-  
-        $types = Type::pluck('icon','name');
 
-        return View('release/show',compact('release','projects','types','release_tickets'));        
+        $types = Type::pluck('icon', 'name');
+
+        return View('release/show', compact('release', 'projects', 'types', 'release_tickets'));
     }
 
     public function store(Request $request)
     {
-        $release = new Release();
+        $release = new Release;
 
         $release->title = $request->title;
         $release->started_at = new Carbon($request->started_at);
@@ -103,9 +100,7 @@ class ReleaseController extends Controller
 
         $release->save();
 
-        \Session::flash('info_message', 'Release Saved');
-
-        return redirect('release/' . $release->id);        
+        return redirect('release/'.$release->id)->with('info_message', 'Release Saved');
 
     }
 }
