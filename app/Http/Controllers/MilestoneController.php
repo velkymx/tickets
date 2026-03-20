5c0f6751d3062320585d792bd1ad8aa2f0328130
Alan B <ajb@wow.com>
2026-03-20 14:32:40 -0700

    Add missing Auth facade import

--- app/Http/Controllers/MilestoneController.php
+++ app/Http/Controllers/MilestoneController.php
@@ -9,6 +9,7 @@ use App\Models\Type;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 
 class MilestoneController extends Controller
