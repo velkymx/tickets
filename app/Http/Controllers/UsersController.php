608251379fd8c08dc0739657e6af16aa4767cb1c
Alan B <ajb@wow.com>
2026-03-20 14:47:58 -0700

    Replace Session::flash with redirect->with in UsersController

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,135 +2,130 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Http\Requests;
-use Illuminate\Support\Facades\Auth;
+use App\Models\Status;
 use App\Models\Ticket;
 use App\Models\TicketUserWatcher;
-use App\Models\Status;
 use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
-
     public function show($id)
     {
 
-      $user = User::findOrFail($id);
+        $user = User::findOrFail($id);
 
-      $statuses = Status::pluck('name','id');      
+        $statuses = Status::pluck('name', 'id');
 
-      foreach($statuses as $status => $val){
+        foreach ($statuses as $status => $val) {
 
-          $alltickets[$val] = Ticket::where('user_id2',$id)->where('status_id',$status)->get();
+            $alltickets[$val] = Ticket::where('user_id2', $id)->where('status_id', $status)->get();
 
-          if(sizeof($alltickets[$val])==0) unset($alltickets[$val]);
+            if (count($alltickets[$val]) == 0) {
+                unset($alltickets[$val]);
+            }
 
-      }
+        }
 
-      $time = new \DateTime(NULL, new \DateTimeZone($user->timezone));
-    
-      // Us dumb Americans can't handle millitary time
-      $ampm = $time->format('H') > 12 ? ' ('. $time->format('g:i a'). ')' : '';
-  
-      // Remove region name and add a sample time
-      $currenttime = $time->format('H:i') . $ampm;      
+        $time = new \DateTime(null, new \DateTimeZone($user->timezone));
 
-      return View('users.show',compact('user','alltickets','currenttime'));
+        // Us dumb Americans can't handle millitary time
+        $ampm = $time->format('H') > 12 ? ' ('.$time->format('g:i a').')' : '';
+
+        // Remove region name and add a sample time
+        $currenttime = $time->format('H:i').$ampm;
+
+        return View('users.show', compact('user', 'alltickets', 'currenttime'));
 
     }
 
     public function edit()
     {
-      
-      $user = User::findOrFail(Auth::id());
 
-      $timezones = $this->get_timezones();
+        $user = User::findOrFail(Auth::id());
 
-      $themes = [
-        '/css/bootstrap.min.css' => 'Default',
-        '/css/bootstrap.darkly.min.css' => 'Darkly'
-      ];
+        $timezones = $this->get_timezones();
 
-      return View('users.edit',compact('user','timezones','themes'));
-      
+        $themes = [
+            '/css/bootstrap.min.css' => 'Default',
+            '/css/bootstrap.darkly.min.css' => 'Darkly',
+        ];
+
+        return View('users.edit', compact('user', 'timezones', 'themes'));
+
     }
 
     public function update(Request $request)
     {
-      $user = User::findOrFail(Auth::id());
-      $user->name = $request->name;
-      $user->email = $request->email;
-      $user->phone = $request->phone;
-      $user->timezone = $request->timezone;
-      $user->theme = $request->theme;
-      $user->title = $request->title;
-      $user->bio = $request->bio;
+        $user = User::findOrFail(Auth::id());
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->phone = $request->phone;
+        $user->timezone = $request->timezone;
+        $user->theme = $request->theme;
+        $user->title = $request->title;
+        $user->bio = $request->bio;
 
-      $user->save();
+        $user->save();
 
-      \Session::flash('info_message', 'Profile Changes Saved');
+        return redirect('users/'.Auth::id())->with('info_message', 'Profile Changes Saved');
 
-      return redirect('users/' . Auth::id());
-
-    }    
+    }
 
     public function watch($id)
     {
 
-      $ticket = Ticket::findOrFail($id);
+        $ticket = Ticket::findOrFail($id);
 
-      $watch = TicketUserWatcher::where('ticket_id',$id)->where('user_id',Auth::id())->first();
+        $watch = TicketUserWatcher::where('ticket_id', $id)->where('user_id', Auth::id())->first();
 
-      if($watch){
+        if ($watch) {
 
-        $watch->delete();
+            $watch->delete();
 
-        $message = 'Watch stopped for this ticket';
+            $message = 'Watch stopped for this ticket';
 
-      } else {
+        } else {
 
-        TicketUserWatcher::create(['user_id'=>Auth::id(),'ticket_id'=>$id]);
+            TicketUserWatcher::create(['user_id' => Auth::id(), 'ticket_id' => $id]);
 
-        $message = 'Watch started for this ticket';
+            $message = 'Watch started for this ticket';
 
-      }
+        }
 
-      return $message;
+        return $message;
 
     }
 
     private function get_timezones()
     {
-      $regions = array(
-        'Africa' => \DateTimeZone::AFRICA,
-        'America' => \DateTimeZone::AMERICA,
-        'Antarctica' => \DateTimeZone::ANTARCTICA,
-        'Aisa' => \DateTimeZone::ASIA,
-        'Atlantic' => \DateTimeZone::ATLANTIC,
-        'Europe' => \DateTimeZone::EUROPE,
-        'Indian' => \DateTimeZone::INDIAN,
-        'Pacific' => \DateTimeZone::PACIFIC
-    );
-    
-    $timezones = array();
-    foreach ($regions as $name => $mask)
-    {
-        $zones = \DateTimeZone::listIdentifiers($mask);
-        foreach($zones as $timezone)
-        {
-        // Lets sample the time there right now
-        $time = new \DateTime(NULL, new \DateTimeZone($timezone));
-    
-        // Us dumb Americans can't handle millitary time
-        $ampm = $time->format('H') > 12 ? ' ('. $time->format('g:i a'). ')' : '';
-    
-        // Remove region name and add a sample time
-        $timezones[$name][$timezone] = substr($timezone, strlen($name) + 1) . ' - ' . $time->format('H:i') . $ampm;
-      }
-    }     
-    
-    return $timezones;
-    }
+        $regions = [
+            'Africa' => \DateTimeZone::AFRICA,
+            'America' => \DateTimeZone::AMERICA,
+            'Antarctica' => \DateTimeZone::ANTARCTICA,
+            'Aisa' => \DateTimeZone::ASIA,
+            'Atlantic' => \DateTimeZone::ATLANTIC,
+            'Europe' => \DateTimeZone::EUROPE,
+            'Indian' => \DateTimeZone::INDIAN,
+            'Pacific' => \DateTimeZone::PACIFIC,
+        ];
 
+        $timezones = [];
+        foreach ($regions as $name => $mask) {
+            $zones = \DateTimeZone::listIdentifiers($mask);
+            foreach ($zones as $timezone) {
+                // Lets sample the time there right now
+                $time = new \DateTime(null, new \DateTimeZone($timezone));
+
+                // Us dumb Americans can't handle millitary time
+                $ampm = $time->format('H') > 12 ? ' ('.$time->format('g:i a').')' : '';
+
+                // Remove region name and add a sample time
+                $timezones[$name][$timezone] = substr($timezone, strlen($name) + 1).' - '.$time->format('H:i').$ampm;
+            }
+        }
+
+        return $timezones;
+    }
 }
