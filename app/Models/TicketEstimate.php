8db55e67bdc1685c962b2e3a0d3a1dc2a98c531c
Alan B <ajb@wow.com>
2026-03-21 09:24:17 -0700

    Add $table, $casts, and type hints to TicketEstimate model

--- app/Models/TicketEstimate.php
+++ app/Models/TicketEstimate.php
@@ -1,24 +1,29 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class TicketEstimate extends Model
 {
+    protected $table = 'ticket_estimates';
 
     protected $fillable = [
-        'user_id', 'ticket_id','storypoints'
+        'user_id', 'ticket_id', 'storypoints',
     ];
 
-    public function user()
+    protected $casts = [
+        'storypoints' => 'integer',
+    ];
+
+    public function user(): BelongsTo
     {
         return $this->belongsTo('App\Models\User');
     }
-  
-    public function ticket()
+
+    public function ticket(): BelongsTo
     {
         return $this->belongsTo('App\Models\Ticket');
-    }    
+    }
 }
