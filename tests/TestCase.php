85003ba31549c7716ddd4d4a1feeda879d69c445
Alan B <ajb@wow.com>
2026-03-23 15:02:54 -0700

    Fix CSRF 419 errors in tests by disabling ValidateCsrfToken middleware
    Tests were failing because Laravel 12's web middleware group includes
    ValidateCsrfToken, which runs before auth middleware. This caused
    419 responses instead of the expected 302 redirects to /login.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,15 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
-abstract class TestCase extends BaseTestCase {}
+abstract class TestCase extends BaseTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutMiddleware(ValidateCsrfToken::class);
+    }
+}
