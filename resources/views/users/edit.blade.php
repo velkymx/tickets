62471bc568507aa51c19c1dae334e44e6d1b30bf
Alan B <ajb@wow.com>
2026-05-20 07:50:02 -0700

    feat: link API docs from API token section

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -80,7 +80,7 @@
 </form>
 
 <h3 class="mt-5 mb-3">API Token</h3>
-<p class="text-muted">Generate a bearer token to authenticate API requests. <a href="https://github.com/velkymx/tickets/blob/master/README.md" target="_blank">View API documentation</a>.</p>
+<p class="text-muted">Generate a bearer token to authenticate API requests. <a href="{{ route('api.docs') }}">View API documentation</a>.</p>
 
 @if (session('api_token_plain'))
     <div class="alert alert-warning">
